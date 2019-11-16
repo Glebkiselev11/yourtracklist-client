23a8f357a9a5275a41b850bf72678f6e8d10ff90
glebkiselev11 <12bny23gleb@gmail.com>
2019-11-16 14:55:14 +0300

    добавил преобразование формата iso даты в более нормальный формат

--- src/components/app/PrevInfo.vue
+++ src/components/app/PrevInfo.vue
@@ -1,7 +1,8 @@
 <template>
   <!-- Компонент информации о релизе / видео которая распологается в предпросмотре (дата/ название и автор )-->
   <div>
-    <span v-if="date" class="release-date">{{date}}</span>
+    <!-- Здесь преобразуем формат времени в ISO в более привлекательный формат -->
+    <span v-if="date" class="release-date">{{new Date(date).toLocaleDateString('en-US', {month: 'long', day: 'numeric', year: 'numeric'})}}</span>
     <router-link :to="permalink" class="release-name">{{name}}</router-link>
 
     <!-- Здесь выводим ссылку на дискографию артиста, если он не один, то через цикл -->
