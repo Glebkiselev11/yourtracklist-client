d81ec7e92f29c31093135daa73b1a7c2d2932f68
glebkiselev11 <12bny23gleb@gmail.com>
2019-12-29 18:05:54 +0300

    доделал стор для поиска

--- src/store/modules/search.js
+++ src/store/modules/search.js
@@ -4,11 +4,23 @@ import axios from 'axios'
 export default {
   actions: {
     async submitSearchQuery({commit}, searchQuery) {
-      console.log(commit)
+      
       try {
-        const {data} = await axios.post('/api/search-query', {searchQuery})
+        const {data : {
+          releases, 
+          videos,
+          authors, 
+          releasesCount, // Кол-во всего найденых релизов
+          videosCount, // кол-во всего найденых видеозаписей
+        }} = await axios.post('/api/search-query', {searchQuery})
 
-        console.log(data)
+        commit('setSearchReleases', releases)
+        commit('setSearchVideos', videos)
+        commit('setSearchAuthors', authors)
+        commit('setSearchReleasesCount', releasesCount)
+        commit('setSearchVideosCount', videosCount)
+
+        console.log(releases, videos, authors, releasesCount, videosCount)
 
       } catch (error) {
         console.log(error)
@@ -17,12 +29,54 @@ export default {
 
   },
   mutations: {
+    setSearchReleases(s, releases) {
+      s.searchReleases = releases
+    },
+    clearSearchReleases(s) {
+      s.searchReleases = null
+    },
+
+    setSearchReleasesCount(s, releasesCount) {
+      s.searchReleasesCount = releasesCount
+    },
+    clearSearchReleasesCount(s) {
+      s.searchReleasesCount = 0
+    },
+
+    setSearchVideos(s, videos) {
+      s.searchVideos = videos
+    },
+    clearSearchVideos(s) {
+      s.searchVideos = null
+    },
+
+    setSearchVideosCount(s, videosCount) {
+      s.searchVideosCount = videosCount
+    },
+    clearSearchVideosCount(s) {
+      s.searchVideosCount = 0
+    },
+
+    setSearchAuthors(s, authors) {
+      s.searchAuthors = authors
+    },
+    clearSearchAuthors(s) {
+      s.searchAuthors = null
+    },
 
   },
   state: {
-    
+    searchReleases: null, // 3 Найденых релиза
+    searchReleasesCount: 0, // Кол-во найденых релизов всего (то что выше это только то что показываем)
+    searchVideos: null, // 3 Найденых видео
+    searchVideosCount: 0, // Кол-во найденхы видео всего
+    searchAuthors: null, // Найденые автора, их мы пока без ограничния показываем всех найденых
   },
   getters: {
-
+    searchReleases: s => s.searchReleases,
+    searchReleasesCount: s => s.searchReleasesCount,
+    searchVideos: s => s.searchVideos,
+    searchVideosCount: s => s.searchVideosCount,
+    searchAuthors: s => s.searchAuthors,
   }
 }
