5c4a8d61b2984864b9c42604cbdfae7b18c8e060
glebkiselev11 <12bny23gleb@gmail.com>
2020-02-09 18:30:03 +0300

    добавил кнопку воспроизведения и паузы

--- src/components/AppTrackItem.vue
+++ src/components/AppTrackItem.vue
@@ -1,8 +1,15 @@
 <template>
   <!-- Компонент предназначен для 1 трека, который можно послушать (будет использоваться в релизах а потом и по отдельности) -->
-    <div>
-      
-      <div>
+    <div class="track-wrap">
+
+      <div class="left-side-track">
+
+        <!-- Кнопка воспроизведение и паузы у трека -->
+        <AppPlayPauseBtn 
+          :isPlay="isPlay"
+          @playAudio="playAudio"
+        />
+
         <span
           v-for="(author, index) of track.authors_permalink"
           :key="index"
@@ -17,7 +24,7 @@
         
           —
         <!-- Название трека -->  
-        <span>{{track.name}}</span>
+        <span class="track-name">{{track.name}}</span>
 
       </div>
 
@@ -31,9 +38,14 @@
 
 <script>
 import duration from '@/mixins/duration.mixin.js'
+import AppPlayPauseBtn from '@/components/AppPlayPauseBtn'
 
 export default {
   name: 'AppTrackItem',
+
+  components: {
+    AppPlayPauseBtn, // Кнопка плей / паузы
+  },
 
   mixins: [
     duration, // Внутри computedDuration он превращает секунды например 90 в 01:30
@@ -41,18 +53,54 @@ export default {
 
   props: {
     track: Object
-  }
+  },
+
+  data: () => ({
+    isPlay: false, // Инфа о том включен трек или нет
+  }),
+
+  methods: {
+    playAudio() { // Воспроизводит аудио файл
+
+      if (this.isPlay == false) {
+        this.isPlay = true // Указываем что мы включили аудио
+        // this.track.audio.play()  // Включаем аудио файл
+      } else {
+        this.isPlay = false // Указываем что мы выключили аудио
+        // this.track.audio.pause() // Ставим на паузу аудио файл
+      }
+    }
+  },
 
 
 }
 </script>
 
 <style sooped>
+  /* Обертка под весь итем */
+  .track-wrap {
+    display: flex;
+    align-items: center;
+  }
+
+  /* Левая часть */
+  .left-side-track {
+    display: flex;
+    align-items: center;
+  }
+
   .author-item {
     font-weight: 500;
+    margin-right: .2em;
+  }
+
+  .track-name {
+    margin-left: .2em;
   }
 
   .author-link:hover {
     border-bottom: 2px solid rgba(0, 0, 0, 0.829);
   }
+
+  
 </style>
